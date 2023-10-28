c0caa81620c2a2e81912b25d857300f4751aa707
devmasud1 <devmahmud.masu1@gmail.com>
2023-10-29 02:33:43 +0600

    single services api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
- require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
@@ -23,6 +23,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const serviceCollection = client.db("geniusCar").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const result = await serviceCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
