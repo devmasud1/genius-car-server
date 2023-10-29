4277d5c4cdb58c1626e9bc04d440db34607e0d32
devmasud1 <devmahmud.masu1@gmail.com>
2023-10-29 13:33:46 +0600

    booking delete api

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/booking/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+    
+
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
